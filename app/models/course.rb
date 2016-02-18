5f7c29f2b70d3ee3247adea86601824f02cb6b5d
mrz <default.mail@mail.ru>
2016-02-18 11:17:48 +0300

    Kaminari per_page to controller.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,5 @@ class Course < ActiveRecord::Base
   validates :title, length: { maximum: 50 }, presence:  true
   validates :description, presence:  true
 
-  paginates_per 5
-
   mount_uploader :image, CourseImageUploader
 end
