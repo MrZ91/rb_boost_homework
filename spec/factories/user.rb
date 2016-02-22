faa1380432d6213779e5e424d119dd244263b3b5
mrz <default.mail@mail.ru>
2016-02-22 18:14:36 +0300

    Some troubles woth using Faker at factorie on CI server.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name {'Example'}
     last_name {'User'}
-    email    { 'example@mail.com' }
+    sequence(email)    { |n| "example#{n}@mail.com" }
     password { '123456' }
     password_confirmation { '123456' }
   end
